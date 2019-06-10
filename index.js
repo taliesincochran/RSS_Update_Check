a2f07ad74b75ff4384107553e1f632913b614273
Taliesin Cochran <taliesincochran@gmail.com>
2019-06-09 21:31:02 -0400

    wrote rss-feed function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin / env node
 'use strict';
-
-
+const testDictionary = require('./test/testDictionary.json')
+const RSSParser = require('rss-parser');
+const parser = new RSSParser();
 // set up a class for RSSSearch
 // We will assume the dictionary will come in the form of JSON
 // As it is a standard form of data transfer on the web and usable
@@ -9,10 +10,13 @@
 
 class RSSSearch {
     constructor(dictionary, numberOfDays) {
-        this.dictionary = JSON.parse(dictionary);
+        this.dictionary = dictionary;
         this.numberOfDays = numberOfDays;
         this.hasUpdated = new Set();
         this.notUpdated = new Set();
+        this.adjustedDate = this.adjustDate();
+        this.adjustedDate = this.adjustDate(numberOfDays, new Date());
+        this.getRSSFeed = this.getRSSFeed.bind(this);
     }
     // Set up with optional parameters to make testing a bit easier
     
@@ -29,9 +33,67 @@ class RSSSearch {
     }
     // JSON supports arrays, strings and strings
     // We will assume that all urls are going to be inside an array
-    
+    async getRSSFeed (company, targetURL) {
+        const { hasUpdated, notUpdated, adjustedDate } = this;
+        const getFeed = async () => {
+            // get the url and parse it
+            let feed = await parser.parseURL(targetURL);
+            // go to the first entry and check its date
+            // it is most likely that it is the newest 
+            // entry, because that is how RSS feeds usually are
+            let compareDate = new Date(feed.items[0].pubDate);
+            console.log(compareDate, '\n', adjustedDate, '\n', compareDate >= adjustedDate);
+            // If the date is within the range, add it to the updated list
+            if(compareDate >= adjustedDate) {
+                if(!hasUpdated.has(company)) {
+                    console.log('has', company);
+                    hasUpdated.add(company);
+                }
+                // also check the not updated list, if it is there
+                // remove it
+                if(notUpdated.has(company)) {
+                    notUpdated.delete(company);
+                }
+            } else {
+                // Add the company to the not updated list if it is not on the
+                // updated list
+                if(!notUpdated.has(company) && !hasUpdated.has(company)) {
+                    console.log('has not', company)
+                    notUpdated.add(company);
+                }
+            }
+            // set the results
+            this.hasUpdated = hasUpdated;
+            this.notUpdated = notUpdated;
+            let data = {hasUpdated, notUpdated}
+            return await data;
+        }
+        let data = await getFeed();
+        return data;
+    }
+    async getFeeds () {
+        let dictionary = JSON.parse(JSON.stringify(this.dictionary));
+        for(let company in dictionary) {
+            let feeds = dictionary[company];
+            for(let feed in feeds) {
+                console.log('feed', dictionary[company][feed][0].date)
+                await this.getRSSFeed(company, dictionary[company][feed]);
+            }
+        }
+    }
 }
-
+let rssSearch = new RSSSearch(testDictionary, 5);
+async function rssSearch2 () {
+    await rssSearch.getFeeds().then(data => {
+        console.log(rssSearch.hasUpdated);
+        console.log(rssSearch.notUpdated);
+    }).catch(error => {
+        console.log(new Error(error));
+    });
+};
+rssSearch2();
+     
+    
 
   // Then we will cycle through the input and retrieve the RSS feed.  
 
@@ -55,3 +117,6 @@ class RSSSearch {
   /* any other RSS feeds for that company, because they have updated one).  Then we repeat with the second, third, ect. */
   // If no RSS feeds are updated, we will add it to the not updated set.
   // Once all companies are checked we will print out a list of companies into the console or print the list.
+
+
+module.exports = RSSSearch;
